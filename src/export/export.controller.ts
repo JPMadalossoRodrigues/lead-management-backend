e632441ccb07c9f8debca684c669772f3e8bb26f
Joao Pedro Madalosso Rodrigues <jpmadalossorodrigues@gmail.com>
2026-04-02 16:36:22 -0300

    ajuste de rota por conflito

--- src/export/export.controller.ts
+++ src/export/export.controller.ts
@@ -2,11 +2,11 @@ import { Controller, Get, Query } from '@nestjs/common';
 import { ExportService } from './export.service';
 import { FilterExportDto } from './dtos/filter-export.dto';
 
-@Controller('leads/export')
+@Controller('export')
 export class ExportController {
   constructor(private readonly exportService: ExportService) {}
 
-  @Get()
+  @Get('leads')
   async export(@Query() query: FilterExportDto) {
     return this.exportService.exportLeads(query);
   }
